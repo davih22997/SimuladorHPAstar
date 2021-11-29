7a11128f9e848429901e7d41b38979fe906fcfd1
David Díaz Roldán <davih22997@gmail.com>
2021-11-29 22:44:42 +0100

    Añado apartado de control simulación Añado función para inicializar botones de iconos más rápidamente Añado variable velocidad para el control de la velocidad (queda implementar) Falta implementar los ActionListener para cada botón

--- src/program/Interfaz.java
+++ src/program/Interfaz.java
@@ -39,24 +39,33 @@ public class Interfaz extends JFrame implements ActionListener {
 	// Control de todas las acciones
 	private JTextArea log;
 
+	// Dimensión para los botones con iconos
+	private Dimension dIcons = new Dimension(16, 16);
+
 	// Parte para la gestión de archivos
 	private File archivo;
 	private JButton btnSave, btnOpen, btnNew;
 	private JFileChooser fc;
 	private JLabel titulo;
 	private JTextField direccion;
-	private Dimension dIcons = new Dimension(16, 16);
 	private JPanel btnPanel;
+
+	// Parte central
+	private JPanel pCentral;
 
 	// Parte para la gestión de algoritmo
 	private JLabel titulo2;
 	private JComboBox<String> algCB;
 	private JPanel algPanel;
-	
-	// Parte para la gestión de la velocidad
+
+	// Parte para el control de la simulación
 	private JLabel titulo3;
+	private JButton btnStart, btnStop;
+	// Control de velocidad
 	private JButton btnMinus, btnPlus;
-	private JButton btnstart, btnstop;
+	private JPanel btnPanel2;
+	// Variable que controla el valor de la velocidad:
+	private int vel = 1;
 
 	public Interfaz() {
 		// ImageIcon st = new ImageIcon(getClass().getResource("/images/play.png"));
@@ -105,22 +114,13 @@ public class Interfaz extends JFrame implements ActionListener {
 		// Damos a cada botón sus características propias:
 
 		// Botón para nuevo archivo
-		btnNew = new JButton();
-		btnNew.setSize(dIcons);
-		btnNew.setIcon(this.createIcon(Direccion.new16, btnNew));
-		btnNew.addActionListener(this);
+		btnNew = initButtonIcon(Direccion.new16);
 
 		// Botón para abrir archivo
-		btnOpen = new JButton();
-		btnOpen.setSize(dIcons);
-		btnOpen.setIcon(this.createIcon(Direccion.open16, btnOpen));
-		btnOpen.addActionListener(this);
+		btnOpen = initButtonIcon(Direccion.open16);
 
 		// Botón para guardar archivo
-		btnSave = new JButton();
-		btnSave.setSize(dIcons);
-		btnSave.setIcon(this.createIcon(Direccion.save16, btnSave));
-		btnSave.addActionListener(this);
+		btnSave = initButtonIcon(Direccion.save16);
 
 		// Barra con la dirección del archivo
 		JLabel dirAct = new JLabel("Actual:");
@@ -142,6 +142,10 @@ public class Interfaz extends JFrame implements ActionListener {
 		vB1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 
 		this.add(vB1, BorderLayout.NORTH);
+
+		// Parte CENTRAL del simulador
+		// Inicializamos el panel central
+		pCentral = new JPanel();
 
 		// Definimos el apartado para la selección de ALGORITMO
 
@@ -169,97 +173,54 @@ public class Interfaz extends JFrame implements ActionListener {
 		vB2.add(algPanel);
 		vB2.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 
-		this.add(vB2, BorderLayout.CENTER);
-		
-		
+		pCentral.add(vB2);
+
 		// Definimos el apartado para el CONTROL SIMULACIÓN
+		titulo3 = new JLabel("Control de la simulación");
+		titulo3.setAlignmentX(CENTER_ALIGNMENT);
+
+		// Añadimos un panel exclusivo para los botones
+		btnPanel2 = new JPanel();
+		// Damos a cada botón sus características propias:
+
+		// Botón para empezar la simulación
+		btnStart = initButtonIcon(Direccion.start16);
+
+		// Botón para finalizar la simulación
+		btnStop = initButtonIcon(Direccion.stop16);
+
+		// Parte de la velocidad
+		JLabel vel = new JLabel("Velocidad:");
+
+		// Botón para reducir la velocidad
+		btnMinus = initButtonIcon(Direccion.minus16);
+
+		// Botón para aumentar la velocidad
+		btnPlus = initButtonIcon(Direccion.plus16);
+
+		// Añadimos todo al panel de botones
+		btnPanel2.add(btnStart);
+		btnPanel2.add(btnStop);
+		btnPanel2.add(vel);
+		btnPanel2.add(btnMinus);
+		btnPanel2.add(btnPlus);
+
+		Box vB3 = Box.createVerticalBox();
+		vB3.add(titulo3);
+		vB3.add(btnPanel2);
+		vB3.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+
+		// Lo añadimos al panel central
+
+		pCentral.add(vB3);
+
+		// Añadimos el panel central
+		this.add(pCentral, BorderLayout.WEST);
 
 		// Empaquetamos y hacemos visible
 		log.append("Iniciamos la interfaz con los valores por defecto." + newline);
 		this.pack();
 		this.setVisible(true);
-
-		/*
-		 * this.setResizable(false); this.setBounds(100, 100, 600, 350);
-		 * 
-		 * JPanel contentPane = new JPanel(); contentPane.setBorder(new EmptyBorder(5,
-		 * 5, 5, 5)); this.setContentPane(contentPane);
-		 * 
-		 * // Definimos el primer submenú (ALGORITMO): Box vB1 =
-		 * Box.createVerticalBox(); vB1.setBorder(new BevelBorder(BevelBorder.LOWERED,
-		 * null, null, null, null));
-		 * 
-		 * JLabel lbl1 = new JLabel("Algoritmo");
-		 * lbl1.setAlignmentX(Component.CENTER_ALIGNMENT); vB1.add(lbl1);
-		 * 
-		 * JComboBox<String> cb1 = new JComboBox<>(); cb1.addItem("A*");
-		 * cb1.addItem("HPA*"); cb1.setToolTipText(""); vB1.add(cb1);
-		 * 
-		 * // Definimos el segundo submenú (HEURÍSTICO): Box vB2 =
-		 * Box.createVerticalBox(); vB2.setBorder(new BevelBorder(BevelBorder.LOWERED,
-		 * null, null, null, null));
-		 * 
-		 * JLabel lbl2 = new JLabel("Heurístico");
-		 * lbl2.setAlignmentX(Component.CENTER_ALIGNMENT); vB2.add(lbl2);
-		 * 
-		 * JComboBox<String> cb2 = new JComboBox<>(); cb2.addItem("DIST-MANHATAN");
-		 * cb2.addItem("DIST-EUCLIDEA"); cb2.addItem("H=0"); vB2.add(cb2);
-		 * 
-		 * // Definimos el tercer menú (CONTROL SIMULACIÓN): Box vB3 =
-		 * Box.createVerticalBox(); vB3.setBorder(new BevelBorder(BevelBorder.LOWERED,
-		 * null, null, null, null));
-		 * 
-		 * JLabel lbl3 = new JLabel("Control simulación");
-		 * lbl3.setAlignmentX(Component.CENTER_ALIGNMENT); vB3.add(lbl3);
-		 * 
-		 * JPanel p1 = new JPanel(); vB3.add(p1);
-		 * 
-		 * JSplitPane sp1 = new JSplitPane(); p1.add(sp1);
-		 * 
-		 * JButton b1 = new JButton("Play"); sp1.setLeftComponent(b1);
-		 * 
-		 * JButton b2 = new JButton("Stop"); sp1.setRightComponent(b2);
-		 * 
-		 * JLabel jl1 = new JLabel("Velocidad"); p1.add(jl1);
-		 * 
-		 * JSplitPane sp2 = new JSplitPane(); p1.add(sp2);
-		 * 
-		 * JButton b3 = new JButton("-"); sp2.setLeftComponent(b3);
-		 * 
-		 * JButton b4 = new JButton("+"); sp2.setRightComponent(b4);
-		 * 
-		 * // Lo añadimos todo al mismo GroupLayout GroupLayout gl = new
-		 * GroupLayout(contentPane);
-		 * gl.setHorizontalGroup(gl.createParallelGroup(Alignment.LEADING).addGroup(gl.
-		 * createSequentialGroup() .addContainerGap()
-		 * .addGroup(gl.createParallelGroup(Alignment.LEADING).addGroup(gl.
-		 * createSequentialGroup() .addComponent(vB1, GroupLayout.PREFERRED_SIZE, 190,
-		 * GroupLayout.PREFERRED_SIZE).addGap(165) .addComponent(vB2,
-		 * GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE)).addComponent(
-		 * vB3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-		 * GroupLayout.PREFERRED_SIZE)) .addContainerGap(GroupLayout.DEFAULT_SIZE,
-		 * Short.MAX_VALUE)));
-		 * gl.setVerticalGroup(gl.createParallelGroup(Alignment.LEADING)
-		 * .addGroup(gl.createSequentialGroup()
-		 * .addGroup(gl.createParallelGroup(Alignment.LEADING) .addComponent(vB2,
-		 * GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-		 * GroupLayout.PREFERRED_SIZE) .addComponent(vB1, GroupLayout.DEFAULT_SIZE,
-		 * GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		 * .addPreferredGap(ComponentPlacement.RELATED).addComponent(vB3,
-		 * GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-		 * GroupLayout.PREFERRED_SIZE) .addGap(204))); // Lo añadimos como contenido de
-		 * la aplicación: contentPane.setLayout(gl);
-		 * 
-		 */
-		/*
-		 * JButton b = new JButton();
-		 * 
-		 * b.setBounds(100, 100, 100, 40); b.setIcon(this.createIcon(stop, b));
-		 * 
-		 * this.add(b); this.setSize(300, 400);
-		 * 
-		 * this.setLayout(null); this.setVisible(true);
-		 */
 	}
 
 	/**
@@ -282,6 +243,36 @@ public class Interfaz extends JFrame implements ActionListener {
 
 		// Devuelve la imagen reescalada
 		return new ImageIcon(icon.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));
+	}
+
+	/**
+	 * Método para crear un botón de un icono
+	 * 
+	 * @param dim Tamaño del botón (dimensiones)
+	 * @param dir Dirección de la imagen del icono
+	 * @return El botón
+	 */
+	protected JButton initButtonIcon(Dimension dim, String dir) {
+		JButton res = new JButton();
+		// Establece el tamaño
+		res.setSize(dim);
+		// Establece el icono
+		res.setIcon(this.createIcon(dir, res));
+		// Además le añade un ActionListener
+		res.addActionListener(this);
+
+		return res;
+	}
+
+	/**
+	 * Método para crear un botón de un icono usando la variable dIcons para las
+	 * dimensiones
+	 * 
+	 * @param dir Dirección de la imagen del icono
+	 * @return El botón
+	 */
+	protected JButton initButtonIcon(String dir) {
+		return initButtonIcon(dIcons, dir);
 	}
 
 	@Override
@@ -372,6 +363,7 @@ public class Interfaz extends JFrame implements ActionListener {
 				log.append("Se ha cancelado el guardado de fichero." + newline);
 			}
 			log.setCaretPosition(log.getDocument().getLength());
+			// Controlador de la parte del algoritmo
 		} else if (e.getSource() == algCB) {
 			String option = algCB.getSelectedItem().toString();
 			if (option.equals("A*")) {
