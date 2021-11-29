39b3b5b0e9702903cda3b2b5a4bb6680ba8b4ed9
David Díaz Roldán <davih22997@gmail.com>
2021-11-29 22:59:42 +0100

    Borradas imágenes innecesarias y añadidos nuevos iconos. Modificado el archivo de direcciones para incluir estos cambios.

--- src/program/Direccion.java
+++ src/program/Direccion.java
@@ -10,13 +10,6 @@ public class Direccion {
 
 	public Direccion() {
 	}
-	
-	// Rutas de las imágenes que se usarán como iconos
-	public final static String start = "/images/start.png";
-	public final static String play = "/images/play.png";
-	public final static String pause = "/images/pause.png";
-	public final static String shutdown = "/images/shutdown.png";
-	public final static String stop = "/images/stop.jpg";
 
 	// Botones para gestor de archivos
 	public final static String save16 = "/images/Save16.gif";
@@ -26,11 +19,14 @@ public class Direccion {
 	public final static String help16 = "/images/Help16.gif";
 	public final static String delete16 = "/images/Delete16.gif";
 	
+	// Botones para gestionar la velocidad
 	public final static String minus16 = "/images/sign_minus.gif";
 	public final static String plus16 = "/images/sign_plus.gif";
 	
 	// Botones para gestor de simulación
 	public final static String stop16 = "/images/Stop16.gif";
-	public final static String start16 = "/images/arrow-blue-right.jpg";
+	public final static String start16 = "/images/Play16.gif";
+	public final static String pause16 = "/images/Pause16.gif";
+	public final static String loader32 = "/images/loader.gif";
 	
 }
