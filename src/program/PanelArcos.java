f6ef713e18b889e56f86c4fd3b4b378c9c921caf
David Díaz Roldán <davih22997@gmail.com>
2022-03-20 20:12:05 +0100

    Dibujado el grafo abstracto. -> Se ha aplicado modificaciones en la clase PanelArcos para ello.

--- src/program/PanelArcos.java
+++ src/program/PanelArcos.java
@@ -1,9 +1,15 @@
 package program;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -16,13 +22,18 @@ public class PanelArcos extends JPanel {
 	// Matriz con los botones (igual que en mapa)
 	private JButton[][] matrizBotones;
 
+	// Copia de la lista de clusters, cuyo contenido se irá modificando
+	private ArrayList<Cluster> clusters;
+
 	public PanelArcos(Mapa mapa, ArrayList<Cluster> clusters) {
+		// 1. Copiamos la lista de clusters
+		this.clusters = (ArrayList<Cluster>) clusters.clone();
 		// 1. Vamos a crear todo el mapa
-		crearMapa(mapa, clusters);
+		crearMapa(mapa);
 
 	}
 
-	private void crearMapa(Mapa mapa, ArrayList<Cluster> clusters) {
+	private void crearMapa(Mapa mapa) {
 		// Obtenemos el total de filas y de columnas en el mapa
 		int total_filas = mapa.getFilas();
 		int total_cols = mapa.getCols();
@@ -91,6 +102,114 @@ public class PanelArcos extends JPanel {
 		button.setContentAreaFilled(false);
 
 		return button;
+	}
+
+	/**
+	 * Método para pintar los distintos arcos
+	 */
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		// Vamos cogiendo los clusters uno a uno
+		for (Cluster c : clusters) {
+			// Y dentro de él, los nodos uno a uno
+			for (Punto p : c.getNodos()) {
+				// 1. Pintamos los nodos en negro
+				pintarNodo(p, g);
+
+				// 2. Pintamos los arcos
+				pintarArcos(p, g);
+			}
+		}
+
+	}
+
+	/**
+	 * Método para pintar de negro un nodo dado
+	 * 
+	 * @param p
+	 * @param g
+	 */
+	private void pintarNodo(Punto p, Graphics g) {
+		// Cogemos el botón que corresponde al nodo
+		JButton b = matrizBotones[p.getFila()][p.getCol()];
+
+		// De él, cogemos todo el rectángulo
+		Rectangle r1 = b.getBounds();
+		Rectangle2D r2d = r1.getBounds2D();
+
+		// Y pintamos de negro su interior
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setPaint(Color.BLACK);
+		g2.fill(r2d);
+	}
+
+	/**
+	 * Método para pintar los arcos de un nodo dado
+	 * 
+	 * @param p
+	 * @param g
+	 */
+	private void pintarArcos(Punto p, Graphics g) {
+
+		// Definimos el color negro para los arcos
+		g.setColor(Color.BLACK);
+
+		// Cogemos el botón que corresponde con el punto
+		JButton b1 = matrizBotones[p.getFila()][p.getCol()];
+
+		// Cogemos las coordenadas del centro del botón
+		Point p1 = centroBoton(b1);
+
+		// Vamos cogiendo los arcos externos
+		for (Punto p_2 : p.getArcosExternos()) {
+			// Cogemos el botón que corresponde con el punto
+			JButton b2 = matrizBotones[p_2.getFila()][p_2.getCol()];
+
+			// Cogemos las coordenadas del centro del botón
+			Point p2 = centroBoton(b2);
+
+			// Dibujamos la línea
+			g.drawLine(p1.x, p1.y, p2.x, p2.y);
+
+			// Borramos de la lista de arcos externos el punto p1 del punto p_2 (para no
+			// repetir)
+			p_2.getArcosExternos().remove(p);
+		}
+
+		// Cogemos ahora los arcos internos
+		for (Edge edge : p.getArcosInternos()) {
+			// Cogemos el punto final del edge
+			Punto p_2 = edge.pfin;
+			// Miramos que exista un camino entre los arcos
+			if (edge.camino.size() > 0) {
+
+				// Cogemos el botón que corresponde
+				JButton b2 = matrizBotones[p_2.getFila()][p_2.getCol()];
+
+				// Cogemos las coordenadas del botón
+				Point p2 = centroBoton(b2);
+
+				// Dibujamos la línea
+				g.drawLine(p1.x, p1.y, p2.x, p2.y);
+				
+			}
+			// Borramos del punto final este edge para no repetir
+			p_2.getArcosInternos().remove(edge);
+		}
+
+	}
+
+	private Point centroBoton(JButton b) {
+		// Cogemos las coordenadas del botón
+		Point p = b.getLocation();
+
+		// Apuntamos al centro
+		p.x += b.getWidth() / 2;
+		p.y += b.getHeight() / 2;
+
+		return p;
 	}
 
 }
